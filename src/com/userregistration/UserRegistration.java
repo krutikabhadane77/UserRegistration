81088a15bfbfdb4b112d365817d8b1bbdf8e4ed6
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-11-15 23:38:27 +0530

    Added: UC2 As a User need to enter a valid Last Name

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,4 +1,5 @@
 //UC1 As a User need to enter a valid First Name
+//UC2 As a User need to enter a valid Last Name
 package com.userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -17,9 +18,24 @@ public class UserRegistration {
             System.out.println(fname + " is not valid");
         }
     }
+
+    public void lastName() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter last name : ");
+        String lname=sc.next();
+        String regex="[A-Z][a-z]{3}";
+        boolean answer = Pattern.matches(regex,lname);
+        if (answer) {
+            System.out.println(lname + " is valid");
+        }
+        else {
+            System.out.println(lname + " is not valid");
+        }
+    }
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
         user.firstName();
+        user.lastName();
     }
 }
 
